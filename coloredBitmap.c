4b3262e690a0c6a0deeaf7431022dc54d7285e51
lynda hamou <148162237+lynda2020@users.noreply.github.com>
2024-11-17 22:23:44 +0100

    Update coloredBitmap.c

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -8,7 +8,21 @@
 //   colored image size Bitmap (based on the unit passed parametter)
 float coloredBitmap(int w, int h, char* unit) {
    // YOUR CODE HERE - BEGIN
-
+int sizeInBits = w * h * 3 * 8;
+   if (strcmp(unit, "bt") == 0) {
+        return (float)sizeInBits; 
+    }
+   else if (strcmp(unit, "ko") == 0) {
+        return (float)sizeInBits / 1024; 
+    } 
+   else if (strcmp(unit, "mo") == 0) {
+        return (float)sizeInBits / (1024 * 1024); 
+    } 
+   else if (strcmp(unit, "go") == 0) {
+        return (float)sizeInBits / (1024 * 1024 * 1024); 
+    }
+   else {
+        return 0
    // YOUR CODE HERE - END
-   return 0;
+
 }
