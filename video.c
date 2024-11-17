1de6db98f82c803fe5e79be90471c163b36cdf51
lynda hamou <148162237+lynda2020@users.noreply.github.com>
2024-11-17 22:31:01 +0100

    Update video.c

--- video.c
+++ video.c
@@ -10,7 +10,29 @@
 //   colored video size (based on the unit passed parametter)
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
    // YOUR CODE HERE - BEGIN
+   
+long long colorSectionSize = w * h * 24 * fps * durationMovie;
+   
+    long long bwSectionSize = w * h * 8 * fps * durationCredits;
+ 
+    long long totalSizeInBits = colorSectionSize + bwSectionSize;
+
+    if (strcmp(unit, "bt") == 0) {
+        return (float)totalSizeInBits; 
+    }
+    else if (strcmp(unit, "ko") == 0) {
+        return (float)totalSizeInBits / 1024; 
+    } 
+    else if (strcmp(unit, "mo") == 0) {
+        return (float)totalSizeInBits / (1024 * 1024); 
+    } 
+    else if (strcmp(unit, "go") == 0) {
+        return (float)totalSizeInBits / (1024* 1024 * 1024); 
+    }
+    else {
+        return -1;
+    }
 
    // YOUR CODE HERE - END
-   return 0;
+
 }
